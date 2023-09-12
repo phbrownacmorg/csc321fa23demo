cb96ae9c42d98c535be9009b3154f4f090088c05
Peter Brown <peter.brown@converse.edu>
2023-09-12 02:31:51 -0400

    Comment changes

--- hello.asm
+++ hello.asm
@@ -21,21 +21,21 @@ section .text                                   ; Code segment
 Start:
  sub   RSP, 8                                   ; Align the stack to a multiple of 16 bytes
 
- sub   RSP, 32                                  ; 32 bytes of shadow space
+ sub   RSP, 32                                  ; 32 bytes of shadow space (MS x64 calling convention)
  mov   ECX, STD_OUTPUT_HANDLE
  call  GetStdHandle
  mov   qword [REL StandardHandle], RAX
  add   RSP, 32                                  ; Remove the 32 bytes
 
  sub   RSP, 32 + 8 + 8                          ; Shadow space + 5th parameter + align stack
-                                                ; to a multiple of 16 bytes
+                                                ; to a multiple of 16 bytes (MS x64 calling convention)
  mov   RCX, qword [REL StandardHandle]          ; 1st parameter
  lea   RDX, [REL Message]                       ; 2nd parameter
  mov   R8, MessageLength                        ; 3rd parameter
  lea   R9, [REL Written]                        ; 4th parameter
  mov   qword [RSP + 4 * 8], NULL                ; 5th parameter
- call  WriteFile                                ; Output can be redirect to a file using >
+ call  WriteFile                                ; Output can be redirected to a file using >
  add   RSP, 48                                  ; Remove the 48 bytes
 
- xor   ECX, ECX
+ xor   ECX, ECX                                 ; Produces 0 for the return code
  call  ExitProcess
