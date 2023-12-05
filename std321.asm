3199e88d35cb6221df17dcb6dd471cbb931b7a76
Peter Brown <peter.brown@converse.edu>
2023-12-05 15:27:28 -0500

    Completed std321.inc, std321.asm

--- std321.asm
+++ std321.asm
@@ -1,9 +1,8 @@
+%define STD321_NO_EXTERNS
 %include "std321.inc"
 
 STD_OUTPUT_HANDLE   EQU -11
 STD_INPUT_HANDLE    EQU -10
-MAX_INPUT_LENGTH    EQU 11                      ; Ten digits and a sign (for 32 bits)
-
 
 extern GetStdHandle                             ; Import external symbols
 extern ReadFile
@@ -11,12 +10,14 @@ extern WriteFile                                ; Windows API functions, not dec
 
 global std321_int_to_str_fn
 global std321_str_to_int_fn
+global std321_read_str_fn
+global std321_write_str_fn
 global std321_read_int_fn
 global std321_write_int_fn
 
 ;; Get a standard handle.  Arguments are the constant for the handle and the
 ;; address where it should be stored.
-%macro  GetHandle   2
+%macro  std321_GetHandle   2
     sub     rsp, 32
     mov     rcx, %1
     call    GetStdHandle
@@ -24,9 +25,32 @@ global std321_write_int_fn
     add     rsp, 32
 %endmacro
 
+;; Get the handle for standard output and leave it in RCX.  If the handle
+;; has previously been gotten, this macro will not re-fetch, but rather
+;; reuse the previous result.
+%macro  std321_GetWriteHandle   0
+    ;; Has the write handle been fetched yet?
+    mov                 rcx, [REL StdOutHandle]
+    cmp                 rcx, 0
+    jnz                 %%WriteHandleFound
+    std321_GetHandle    STD_OUTPUT_HANDLE, [REL StdOutHandle]
+    mov                 rcx, [REL StdOutHandle]
+    %%WriteHandleFound:
+%endmacro
+
+%macro  std321_GetReadHandle    0
+    ;; Has the read handle been fetched yet?
+    mov                 rcx, [REL StdInHandle]
+    cmp                 rcx, 0
+    jnz                 %%ReadHandleFound
+    std321_GetHandle    STD_INPUT_HANDLE, [REL StdInHandle]
+    mov                 rcx, [REL StdInHandle]
+    %%ReadHandleFound:
+%endmacro
+
 section .data
-    StdOutHandle   dq   0
-    StdInHandle    dq   0
+    StdOutHandle    dq  0
+    StdInHandle     dq  0
 
 section .text
 
@@ -65,7 +89,7 @@ std321_int_to_str_fn:
         jg      .CountDigits
     ;; RCX now holds the number of digits in the number
     add     r11, rcx    ; R11 = R11 + RCX - 1 (next line)
-    dec     rdi         ; R11 now points to the place for the *last* digit
+    dec     r11         ; R11 now points to the place for the *last* digit
     mov     r10, 10     ; Divisor
     mov     rdx, 0      ; Clear out rdx before the first division
 
@@ -83,7 +107,7 @@ std321_int_to_str_fn:
     jge     .NoMinus
     inc     rcx                  ; One more character for the minus sign
 
-    .NoMinus:
+.NoMinus:
     mov     rax, rcx             ; Put the return value (number of bytes) into EAX
     ret
 
@@ -111,22 +135,22 @@ std321_str_to_int_fn:
     sub     rcx, 2                  ; If CR/LF are present, subtract 2 from RCX
     .NoCrLf:
 
-    mov     r8, 0       ; clear R8
-    mov     r9, 1       ; Sign
-    mov     r10, 10     ; Base 10; value in R10 to allow multiplying
+    mov     r8, 0                   ; clear R8
+    mov     r9, 1                   ; Sign
+    mov     r10, 10                 ; Base 10; value in R10 to allow multiplying
 
     ;; Handle the sign character (if any)                         
-    jrcxz   .endLoop    ; Make sure there are, in fact, characters to read
-    mov     r8b, [r11]  ; Look at the first char
+    jrcxz   .endLoop                ; Make sure there are, in fact, characters to read
+    mov     r8b, [r11]              ; Look at the first char
     cmp     r8b, ASCII_MINUS
-    jne     .Loop       ; If no minus sign, pretend we didn't even look
+    jne     .Loop                   ; If no minus sign, pretend we didn't even look
     ;; r8b == '-'. Store the fact that we saw a '-' character.
-    neg     r9          ; Sign <- -1
-    dec     rcx         ; Consumed a character
+    neg     r9                      ; Sign <- -1
+    dec     rcx                     ; Consumed a character
     inc     r11
 
     ;; Main loop
-    jrcxz   .endLoop    ; if CX <= 0, jump to the end of the loop
+    jrcxz   .endLoop                ; if CX <= 0, jump to the end of the loop
     ;; while R8 > 0
     .Loop:
         mul     r10                 ; RAX *= 10 (previous digits)
@@ -135,10 +159,97 @@ std321_str_to_int_fn:
         add     rax, r8             ; Add in the current digit
         inc     r11                 ; Point RSI at the next digit
         loop    .Loop               ; If RCX > 0, decrement RCX and do it again
-    .endLoop:                        ; End the loop
-    imul  r9                         ; Result *will* fit in RAX
+    .endLoop:                       ; End the loop
+    imul  r9                        ; Result *will* fit in RAX
 
     ;; Exit code (epilogue)
+    ret
+
+;;; Function std321_write_str_fn
+;;; Arguments are address of string and length of string.  The number of bytes
+;;; written is returned in RAX.
+;;; If the standard write handle has not been gotten previously, this function
+;;; will get it and store it for future use.
+std321_write_str_fn:
+    ParamsToShadow
+    ;; Establish a stack frame
+    push    rbp
+    mov     rbp, rsp
+    ;; Make space for a local variable on the stack
+    sub     rsp, 8
+    ;; Stack frame:
+    ;; BYTE_COUNT           [rbp - 8]
+    ;; (old RBP):           [rbp]
+    ;; (return address):    [rbp + 8]
+    ;; STRING_ADDR          [rbp + 16]
+    ;; STRING_LENGTH        [rbp + 24]
+
+    std321_GetWriteHandle
+    ;; Print the prompt
+    sub     rsp, (32 + 8 + 8)   ; Shadow space, leaving room the fifth parameter
+                                ; and 8 more bytes for 16-byte alignment.
+    ;; Handle is already in RCX
+    mov     rdx, [rbp + 16]
+    mov     r8,  [rbp + 24]
+    lea     r9,  [rbp - 8]
+    mov     FIFTH_PARAM, NULL
+    call    WriteFile           ; Output can be redirected to a file using >
+    add     rsp, 48
+
+    ;; Put bytes written into RAX
+    mov     rax, [rbp - 8]
+
+    ;; Exit code
+    ;; Get rid of local variable space
+    add     rsp, 8
+    ;; Pop non-volatile register
+    pop     rbp
+    ret
+
+;;; Function std321_read_str_fn
+;;; Arguments are address of prompt, length of prompt, address of string space,
+;;; and maximum length of input.
+;;; If the standard handles have not been gotten previously, this function
+;;; will get them and store them for future use.
+std321_read_str_fn:
+    ParamsToShadow
+    ;; Establish a stack frame
+    push    rbp
+    mov     rbp, rsp
+    ;; Make space for a local variable on the stack
+    sub     rsp, 8
+    ;; Stack frame:
+    ;; BYTES_READ        [rbp - 8]
+    ;; (old RBP):        [rbp]
+    ;; (return address): [rbp + 8]
+    ;; PROMPT_ADDR       [rbp + 16]
+    ;; PROMPT_LENGTH     [rbp + 24]
+    ;; STRING_ADDR       [rbp + 32]
+    ;; MAX_LENGTH        [rbp + 40]
+
+    ;; Print the prompt
+    write_str_no_lea    [rbp+16], [rbp+24]
+
+    ;; Read the string
+    std321_GetReadHandle
+    sub     rsp, (32 + 8 + 8)   ; Shadow space, leaving room the fifth parameter
+                                ; and 8 more bytes for 16-byte alignment.
+    ;; Handle is already in RCX
+    mov     rdx, [rbp + 32]
+    mov     r8, [rbp + 40]
+    lea     r9, [rbp - 8]
+    mov     FIFTH_PARAM, NULL
+    call    ReadFile
+    add     rsp, 48
+
+    ;; Put bytes read into RAX
+    mov     rax, [rbp - 8]
+
+    ;; Exit code
+    ;; Get rid of local variable space
+    add     rsp, 8
+    ;; Pop non-volatile register
+    pop     rbp
     ret
 
 ;;; Function std321_read_int_fn
@@ -152,65 +263,29 @@ std321_read_int_fn:
     push    rbp
     mov     rbp, rsp
     ;; Make space for local variables on the stack
-    sub     rsp, 8 * ((MAX_INPUT_LENGTH + 2) + 1)
+    sub     rsp, 8 * ((MAX_INT_LENGTH + 2) + 1)
     ;; Local variable addresses
-    %define BYTE_COUNT      [rbp - 112]
-    %define STRING_SPACE    [rbp - 104]
-    ;; (old RBP): [rbp]
+    ;; BYTE_COUNT        [rbp - 112]
+    ;; STRING_SPACE      [rbp - 104]
+    ;; (old RBP):        [rbp]
     ;; (return address): [rbp + 8]
-    %define PROMPT_ADDR     [rbp + 16]
-    %define PROMPT_LENGTH   [rbp + 24]
+    ;; PROMPT_ADDR       [rbp + 16]
+    ;; PROMPT_LENGTH     [rbp + 24]
 
-    ;; Has the write handle been fetched yet?
-    mov     rcx, [REL StdOutHandle]
-    jnz     .WriteHandleFound
-    GetHandle   STD_OUTPUT_HANDLE, [REL StdOutHandle]
-    mov     rcx, [REL StdOutHandle]
-    .WriteHandleFound:
-
-    ;; Print the prompt
-    sub     rsp, (32 + 8 + 8)   ; Shadow space, leaving room the fifth parameter
-                                ; and 8 more bytes for 16-byte alignment.
-    ;; Handle is already in RCX
-    mov     rdx, PROMPT_ADDR
-    mov     r8, PROMPT_LENGTH
-    lea     r9, BYTE_COUNT
-    mov     FIFTH_PARAM, NULL
-    call    WriteFile           ; Output can be redirected to a file using >
-    sub     rsp, 48
-
-    ;; Has the read handle been fetched yet?
-    mov     rcx, [REL StdInHandle]
-    jnz     .ReadHandleFound
-    GetHandle   STD_INPUT_HANDLE, [REL StdInHandle]
-    mov     rcx, [REL StdInHandle]
-    .WriteHandleFound:
-    
-    ;; Read the integer
-    sub     rsp, (32 + 8 + 8)   ; Shadow space, leaving room the fifth parameter
-                                ; and 8 more bytes for 16-byte alignment.
-    ;; Handle is already in RCX
-    lea     rdx, STRING_SPACE
-    mov     r8, MAX_INPUT_LENGTH
-    lea     r9, BYTE_COUNT
-    mov     FIFTH_PARAM, NULL
-    call    ReadFile
-    sub     rsp, 48
+    ;; Print the prompt and read the string
+    read_str_one_lea    [rbp + 16], [rbp + 24], [rbp - 104], MAX_INT_LENGTH
+    ;; Returns number of bytes read in RAX
 
     ;; Convert to int
-    str_to_int STRING_SPACE, BYTES_READ
+    str_to_int [rbp - 104], rax
     ;; Leaves result in RAX
 
     ;; Exit code
     ;; Get rid of local variable space
-    add   rsp, 8 * ((MAX_INPUT_LENGTH + 2) + 1)
+    add   rsp, 8 * ((MAX_INT_LENGTH + 2) + 1)
     ;; Pop non-volatile register
     pop   rbp
     ret
-    %undef  BYTE_COUNT
-    %undef  STRING_SPACE
-    %undef  PROMPT_ADDR
-    %undef  PROMPT_LENGTH
 
 ;;; Function std321_write_int_fn
 ;;; Used by write_int macro in std321.inc.
@@ -224,68 +299,42 @@ std321_write_int_fn:
     push    rbp
     mov     rbp, rsp
     ;; Make space for local variables on the stack
-    sub     rsp, 8 * ((MAX_INPUT_LENGTH + 2) + 1)
+    sub     rsp, 8 * ((MAX_INT_LENGTH + 2) + 1)
     ;; Local variable addresses
-    %define BYTE_COUNT      [rbp - 112]
-    %define STRING_SPACE    [rbp - 104]
-    ;; (old RBP): [rbp]
-    ;; (return address): [rbp + 8]
-    %define INT_TO_PRINT    [rbp + 16]
-    %define LABEL_ADDR      [rbp + 24]
-    %define LABEL_LENGTH    [rbp + 30]
-
-    ;; Has the write handle been fetched yet?
-    mov     rcx, [REL StdOutHandle]
-    jnz     .WriteHandleFound
-    GetHandle   STD_OUTPUT_HANDLE, [REL StdOutHandle]
-    mov     rcx, [REL StdOutHandle]
-    .WriteHandleFound:
+    ;; BYTE_COUNT           [rbp - 112]
+    ;; STRING_SPACE         [rbp - 104]
+    ;; (old RBP):           [rbp]
+    ;; (return address):    [rbp + 8]
+    ;; INT_TO_PRINT         [rbp + 16]
+    ;; LABEL_ADDR           [rbp + 24]
+    ;; LABEL_LENGTH         [rbp + 32]
 
     ;; Print the label
-    sub     rsp, (32 + 8 + 8)   ; Shadow space, leaving room the fifth parameter
-                                ; and 8 more bytes for 16-byte alignment.
-    ;; Handle is already in RCX
-    mov     rdx, LABEL_ADDR
-    mov     r8, LABEL_LENGTH
-    lea     r9, BYTE_COUNT
-    mov     FIFTH_PARAM, NULL
-    call    WriteFile           ; Output can be redirected to a file using >
-    sub     rsp, 48
+    write_str_no_lea        [rbp + 24], [rbp + 32]
 
     ;; Convert number to string
-    int_to_str  INT_TO_PRINT, STRING_SPACE
-    mov         BYTE_COUNT, rax
+    int_to_str  [rbp + 16], [rbp - 104]
+    mov         [rbp - 112], rax
 
     ;; Print the number
-    lea         r11, STRING_SPACE
-    add         r11, BYTE_COUNT
-    sub         r11, 2
+    ;; First, add a CR/LF if needed
+    lea         r11, [rbp - 104]        ; Start of the string
+    add         r11, [rbp - 112]        ; End of the string
+    sub         r11, 2                  ; Jump back 2 bytes
     cmp         byte [r11], ASCII_CR    ; Looking at a CR?
     jz          .CrLfAdded              ; If not...
-    add         r11, 2                  ; Set r11 to point to the end of the string
-    mov         word [r11], '0Ah 0Dh'   ; Put the CR/LF on the end
-    add         BYTE_COUNT, 2           ; add to BYTE_COUNT for the CR/LF
+    add         r11, 2                  ; Set r11 back to the end of the string
+    mov         byte [r11], 0Ah         ; Put the CR/LF on the end
+    inc         r11
+    mov         byte [r11], 0Dh     
+    add         [rbp - 112], dword 2    ; add to BYTE_COUNT for the CR/LF
     
-    .CrLfAdded:
-    sub   RSP, 32 + 8 + 8               ; Shadow space + 5th parameter + align stack
-                                        ; to a multiple of 16 bytes (MS x64 calling convention)
-    mov   rcx, OUT_HANDLE               ; Parameter 1: output handle
-    lea   rdx, STRING_SPACE             ; Parameter 2: address of the number string
-    mov   r8, BYTE_COUNT                ; Parameter 3: length of the string
-    lea   r9, BYTE_COUNT                ; Parameter 4: address for bytes written
-    mov   FIFTH_PARAM, NULL
-    call  WriteFile                     ; Output can be redirected to a file using >
-    add   RSP, 48                       ; Remove the 48 bytes shadow space for WriteFile
+.CrLfAdded:
+    write_str   [rbp - 104], [rbp - 112]
 
     ;; Exit code
     ;; Get rid of local variable space
-    add   rsp, 8 * ((MAX_INPUT_LENGTH + 2) + 1)
+    add   rsp, 8 * ((MAX_INT_LENGTH + 2) + 1)
     ;; Pop non-volatile register
     pop   rbp
     ret
-    ;; Undefine the function-local macros
-    %undef BYTE_COUNT
-    %undef STRING_SPACE
-    %undef INT_TO_PRINT
-    %undef LABEL_ADDR
-    %undef LABEL_LENGTH
